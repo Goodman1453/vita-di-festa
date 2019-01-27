9f61bb1140083f3703b65b2265fb4f40a5891f2c
goodman <Goodman.Khumalo@standardbank.co.za>
2019-01-27 03:36:06 +0200

    basic firebase login and sign up implementation

---
+++ app/src/main/java/com/example/goodman/budgetwallet/ForgotPasswordActivity.kt
@@ -0,0 +1,39 @@
+package com.example.goodman.budgetwallet
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    private lateinit var nameAndSurnameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var submitBtn: Button
+    private lateinit var passwordEditText: EditText
+    private lateinit var confirmPasswordEditText: EditText
+
+
+      override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+          emailEditText = sign_up_email
+          submitBtn = sign_up_button
+          nameAndSurnameEditText = name_and_surname
+          passwordEditText = sign_up_password
+          confirmPasswordEditText = confirm_password
+
+          passwordEditText.visibility = View.GONE
+          confirmPasswordEditText.visibility = View.GONE
+          submitBtn.text = "SUBMIT"
+
+          submitBtn.setOnClickListener {
+              //                  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+              Log.d("SubmitBtn", "email submitted")
+          }
+
+      }
+}
--- app/src/main/java/com/example/goodman/budgetwallet/SignUpActivity.kt
+++ app/src/main/java/com/example/goodman/budgetwallet/SignUpActivity.kt
@@ -2,11 +2,88 @@ package com.example.goodman.budgetwallet
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var usernameAndSurname: TextView
+    private lateinit var signUpbtn: Button
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        firebaseAuth = FirebaseAuth.getInstance()
+        signUpbtn = sign_up_button
+        signUpbtn.setOnClickListener {
+            attemptSignUp()
+        }
+    }
+
+    private fun attemptSignUp() {
+
+        sign_up_email.error = null
+        sign_up_password.error = null
+
+        // Store values at the time of the login attempt.
+        val emailStr = sign_up_email.text.toString()
+        val passwordStr = sign_up_password.text.toString()
+        val confirmPasswordStr = confirm_password.text.toString()
+
+        var cancel = false
+        var focusView: View? = null
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(passwordStr) && !isPasswordValid(passwordStr)) {
+            password.error = getString(R.string.error_invalid_password)
+            focusView = password
+            cancel = true
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(emailStr)) {
+            email.error = getString(R.string.error_field_required)
+            focusView = email
+            cancel = true
+        } else if (!isEmailValid(emailStr)) {
+            email.error = getString(R.string.error_invalid_email)
+            focusView = email
+            cancel = true
+        }
+
+        if (cancel) {
+            focusView?.requestFocus()
+        } else {
+            firebaseAuth.createUserWithEmailAndPassword(emailStr, passwordStr)
+                    .addOnCompleteListener { task ->
+                        if(task.isSuccessful){
+                            val user = firebaseAuth.currentUser
+                            updateUI(user)
+                        }
+                    }
+        }
+
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        //TODO: Replace this with your own logic
+        return email.contains("@")
+    }
+    //
+    private fun isPasswordValid(password: String): Boolean {
+        //TODO: Replace this with your own logic
+        return password.length > 4 && sign_up_password.text.toString() == confirm_password.text.toString()
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        Log.d("x", "trial1")
     }
 }
